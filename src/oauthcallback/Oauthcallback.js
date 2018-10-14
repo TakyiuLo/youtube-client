33e4c8f7d5ce3b17fae8de4931097748611620a7
Takyiu Lo <takyiulo@gmail.com>
2018-10-14 08:28:43 -0400

    Add in componentDidMount to  authcallback

--- src/oauthcallback/Oauthcallback.js
+++ src/oauthcallback/Oauthcallback.js
@@ -5,31 +5,46 @@ import { withRouter } from 'react-router-dom'
 class Oauthcallback extends Component {
   constructor() {
     super()
+    
+    this.state = {
+      authenticated: false
+    }
   }  
 
-  render () {
+  componentDidMount () {
+    const { flash, location } = this.props
     // console.log('this.props', this.props)
     // console.log('this.props.location.search', this.props.location.search)
     // remove '?' at the beginning
-    // const search = this.props.location.search.substr(1)
-    // // split by '&'
-    // const searchParamsArr = search.split('&')
-    // const hashSearchParams = {}
-    // searchParamsArr.map((param) => {
-    //   // split by '='
-    //   const keyValueArr = param.split('=')
-    //   //  hash it
-    //   hashSearchParams[keyValueArr[0]] = keyValueArr[1]
-    //   // leave it unmodify
-    //   return param
-    // })
-    // // console.log('hash', hashSearchParams)
-    // // console.log('Window', window)
-    // window.opener && window.opener.postMessage(hashSearchParams, '*')
-    
+    const search = location.search.substr(1)
+    // split by '&'
+    const searchParamsArr = search.split('&')
+    const hashSearchParams = {}
+    searchParamsArr.map((param) => {
+      // split by '='
+      const keyValueArr = param.split('=')
+      //  hash it
+      hashSearchParams[keyValueArr[0]] = keyValueArr[1]
+      // leave it unmodify
+      return param
+    })
+    // check if there is a parent window
+    window.opener ?
+      () => {
+        window.opener && window.opener.postMessage(hashSearchParams, '*')
+        this.setState({ authenticated: true })
+      }
+      :() => flash('No parent window found, Please try again', 'flash-error')
+  }
+
+  render () {
+    const { authenticated } = this.state
+  
     return (
       <div className="container m-5 p-2">
-        Authorization Granted
+        {authenticated ?
+          'Authorization Granted'
+          :'Authorization Denied'}
       </div>
     )
   }
